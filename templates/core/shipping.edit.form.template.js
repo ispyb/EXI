d11bc67e9a850c8a819cf3f4b440c75df5510716
Alberto Nardella <Alberto.Nardella@maxiv.lu.se>
2020-06-18 11:22:45 +0200

    Adding a regex in order to have a prefix in the shipment name (only valid for MAXIV)

--- templates/core/shipping.edit.form.template.js
+++ templates/core/shipping.edit.form.template.js
@@ -3,9 +3,6 @@
         <label class="col-md-2 col-form-label">Name:</label>
         <div class="col-md-10">
             <input id="{id}-name" class="form-control" type="text" value="{shipment.shippingName}">
-            {@eq key="{.showRegEx}" value="true"}
-            <span>{.regExLabel}</span>
-            {/eq}
         </div>
     </div>
     <div class="form-group row">
@@ -46,28 +43,3 @@
         </div>
     </div>
 </div>
-
-{@eq key="{.showRegEx}" value="true"}
-<script>
-    var proposal = "{.proposal}";
-    var regex = "^" +proposal +"-(19|20)\\d\\d(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])-[a-zA-Z-]*";
-    console.log("regex is " +regex);
-    var validate_name = function(name){
-        var is_name_valid = false;
-        if(name.match(regex) != null){
-            is_name_valid = true;
-        }
-        return is_name_valid;
-    };
-
-    $("#{id}-name").on("focusout", function(){
-        var input_val = $(this).val();
-        console.log("input to validate:" +input_val);
-        var is_success = validate_name(input_val);
-        if(!is_success){
-            $("#{id}-name").focus();
-            alert("Error in the name!");
-        }
-    });
-</script>
-{/eq}
